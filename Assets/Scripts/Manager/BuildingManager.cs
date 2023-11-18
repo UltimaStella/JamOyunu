585aca35b88cf0524486761c8fb630d4caba984b
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-11-18 13:25:09 +0300

    Object Pooling is over

---
+++ Assets/Scripts/Manager/BuildingManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+
+public class BuildingManager : MonoBehaviour
+{
+
+    public GameObject[] objects;
+
+    private Vector3 pos;
+
+    private RaycastHit hit;
+
+    [SerializeField] private LayerMask layerMask;
+
+
+    private void FixedUpdate()
+    {
+        Ray hit Camera.main.ScreenPointToRay(Input.)
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
